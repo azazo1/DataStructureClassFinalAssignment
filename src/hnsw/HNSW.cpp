da0cac48b44a833eab7082243305ed26cc353012
azazo1 <azazo1@qq.com>
2024-05-31 23:10:58 +0800

    - 小修改

--- src/hnsw/HNSW.cpp
+++ src/hnsw/HNSW.cpp
@@ -36,10 +36,13 @@ namespace hnsw {
         layer0.changeSearchBatch();
 
         // 先在最稀疏的 layer3 中搜索.
-        int nearestPt = layer3.searchNearestNode(*ptVec, 0, target); // 只需要一个
+        int nearestPt = layer3.searchNearestNode(*ptVec, 0, target);
         nearestPt = layer2.searchNearestNode(*ptVec, nearestPt, target);
         nearestPt = layer1.searchNearestNode(*ptVec, nearestPt, target);
         nearestPt = layer0.searchNearestNode(*ptVec, nearestPt, target);
+
+        // 与上面四行做对比实验, 发现在 8000 数据集下对速度的影响不大.
+        // int nearestPt = layer0.searchNearestNode(*ptVec, 0, target);
 
         layer0.changeSearchBatch(); // inQueue 针对的队列发生变化
         return layer0.searchKNN(*ptVec, nearestPt, target, k); // 搜索到最接近的向量后拓展为 topk
